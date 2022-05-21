4d74397b0dd9e0c35e08a84fe9d8377867de229a
MinhLee2k <minh.nguyenle1809@gmail.com>
2022-05-21 13:03:11 +0700

    feature: Create callball cycle for chatbot

--- src/hooks/useChatbot/common/enum.js
+++ src/hooks/useChatbot/common/enum.js
@@ -1,6 +1,9 @@
 export const gotoLinkMsg = 'Buy in link ';
 export const link = 'http://localhost:3000/product/';
 export const max = 10;
+
+export const locationCallback = 0
+
 export const defaultParamsEnv = {
   name: '',
   productWantToBuy: [
--- src/hooks/useChatbot/hooks/useRunLifeCycleChatbot.js
+++ src/hooks/useChatbot/hooks/useRunLifeCycleChatbot.js
@@ -1,52 +1,50 @@
 import CLifeCycleChatbot from '../Object/CLifeCycleChatbot';
 
 import useContextChatbot from './useContextChatbot';
-import { defaultParamsEnv } from '../common/enum';
-
-async function callbackProtocolLevel1(func, lifeCycleChatbot) {
-  const switchCallback = await func(); // _productWantToBuyRedux
-
-  const confirm = await lifeCycleChatbot.confirmAnswer(
-    switchCallback.map((product) => product.text)
-  );
-  if (confirm) {
-    return switchCallback;
-  } else {
-    await callbackProtocolLevel1(func, lifeCycleChatbot);
-  }
-}
+import { defaultParamsEnv, locationCallback } from '../common/enum';
 
 async function useRunLifeCycleChatbot(createMessage) {
   const state = {
     ...defaultParamsEnv,
   };
-  console.log('..............', state);
   const lifeCycleChatbot = new CLifeCycleChatbot(createMessage);
-
   const { dispatchChatbot, SET_NAME, SET_PRODUCT_WANT_TO_BUY } =
     useContextChatbot(state);
 
-  const nameRedux = await lifeCycleChatbot.askName();
-  dispatchChatbot(SET_NAME(nameRedux));
+  const runLifeCycle = async (_locationCallback = locationCallback) => {
+    if (1 > _locationCallback) {
+      const nameRedux = await lifeCycleChatbot.askName();
+      dispatchChatbot(SET_NAME(nameRedux));
+    }
 
-  const productWantToBuyRedux = await callbackProtocolLevel1(async () => {
-    const _productWantToBuyRedux = await lifeCycleChatbot.askProductWantToBuy(
-      state.name
-    );
-    dispatchChatbot(SET_PRODUCT_WANT_TO_BUY(_productWantToBuyRedux));
+    if (2 > _locationCallback) {
+      const _productWantToBuyRedux = await lifeCycleChatbot.askProductWantToBuy(
+        state.name
+      );
+      dispatchChatbot(SET_PRODUCT_WANT_TO_BUY(_productWantToBuyRedux));
+    }
 
-    return _productWantToBuyRedux;
-  }, lifeCycleChatbot);
+    if (3 > _locationCallback) {
+      const confirm = await lifeCycleChatbot.confirmAnswer(
+        state.productWantToBuy.map((product) => product.text)
+      );
+      if (!confirm) {
+        await runLifeCycle(1);
+      }
+    }
 
-  const isBuyAnother = await lifeCycleChatbot.sendLinkToProduct(
-    productWantToBuyRedux
-  );
+    if (4 > _locationCallback) {
+      const isBuyAnother = await lifeCycleChatbot.sendLinkToProduct(
+        state.productWantToBuy
+      );
+      // if (isBuyAnother) {
+      //   location.reload()
+      // };
+    }
+    console.log('..............', state);
+  }
 
-  // if (isBuyAnother) {
-  //   location.reload()
-  // };
-
-  console.log('..............', state);
+  await runLifeCycle();
 }
 
 export default useRunLifeCycleChatbot;
