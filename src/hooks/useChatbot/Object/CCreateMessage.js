ceead2fb91602136c892ba27e5125430d94194f4
MinhLee2k <minh.nguyenle1809@gmail.com>
2022-05-21 12:29:38 +0700

    reflactor: Move array data quesion/answer into input

--- src/hooks/useChatbot/Object/CCreateMessage.js
+++ src/hooks/useChatbot/Object/CCreateMessage.js
@@ -1,13 +1,10 @@
-import { gotoLinkMsg, link, max, defaultParamsEnv } from "../common/enum"
-import { createLinkToProducts, createListMsgsChatbot, random } from '../common/utils';
-
 export default class CCreateMessage {
   constructor(chatCtrl) {
     this.chatCtrl = chatCtrl
   }
 
-  async createQuestion(index, paramsFromEnv) {
-    await mockQuestions(paramsFromEnv)[index].forEach(async (msg) => {
+  async createQuestion(questions) {
+    await questions.forEach(async (msg) => {
       await this.chatCtrl.addMessage({
         type: msg[0],
         content: msg[1],
@@ -15,9 +12,7 @@ export default class CCreateMessage {
     })
   }
 
-  async createAnswer(indexAnswer, paramsFromEnv) {
-    const answer = mockAnswers(paramsFromEnv)[indexAnswer]
-
+  async createAnswer(answer) {
     if (answer.length === 1)
       return await this.chatCtrl.setActionRequest({
         type: answer[0],
@@ -27,86 +22,4 @@ export default class CCreateMessage {
       options: answer[1],
     });
   }
-}
-
-export function mockQuestions(paramsFromEnv = defaultParamsEnv) {
-  return [
-    [
-      [
-        "text", "Hello, What's your name?"
-      ]
-    ],
-    [
-      [
-        "text", `Hello, ${paramsFromEnv.name}`
-      ],
-      [
-        "text", "What do you want to buy in my store?"
-      ],
-    ],
-    [
-      [
-        "text", `OK, you want to buy ${paramsFromEnv.productWantToBuy.toString()}`
-      ],
-      [
-        "text", "Please confirm it!"
-      ]
-    ],
-    createListMsgsChatbot(createLinkToProducts(paramsFromEnv.productWantToBuy.map(product => product.value), max), gotoLinkMsg, link)
-  ]
-}
-
-export function mockAnswers(paramsFromEnv = defaultParamsEnv) {
-  return [
-    [
-      "text"
-    ],
-    [
-      "multi-select",
-      [
-        {
-          value: 'woman_product_',
-          text: `Woman's outfit`,
-        },
-        {
-          value: 'man_product_',
-          text: `Man's outfit`,
-        },
-        {
-          value: 'bag_product_',
-          text: 'Bag',
-        },
-        {
-          value: 'shoes_product_',
-          text: 'Shoes',
-        },
-        {
-          value: 'watches_product_',
-          text: 'Watches',
-        },
-      ]
-    ],
-    [
-      "select",
-      [
-        {
-          value: '-',
-          text: 'Yes'
-        },
-        {
-          value: '',
-          text: 'No'
-        },
-      ]
-    ],
-    [
-      "multi-select",
-      [
-        {
-          value: 'buy_another',
-          text: `Buy another`,
-        }
-      ]
-    ],
-  ]
 }
--- src/hooks/useChatbot/Object/CLifeCycleChatbot.js
+++ src/hooks/useChatbot/Object/CLifeCycleChatbot.js
@@ -1,45 +1,101 @@
+import { gotoLinkMsg, link, max } from "../common/enum"
+import { createLinkToProducts, createListMsgsChatbot, random } from '../common/utils';
+
 export default class CLifeCycleChatbot {
-  constructor(createMessage, paramsChatbot) {
-    this.createMessage = createMessage,
-      this.paramsChatbot = paramsChatbot
+  constructor(createMessage) {
+    this.createMessage = createMessage
   }
 
   async protocol0() {
-    await this.createMessage.createQuestion(0)
-    const name = await this.createMessage.createAnswer(0)
+    await this.createMessage.createQuestion([
+      [
+        "text", "Hello, What's your name?"
+      ]
+    ])
+    const name = await this.createMessage.createAnswer(["text"])
 
     return name.value
   }
 
   async protocol1(nameRedux) {
-    await this.createMessage.createQuestion(1, {
-      ...this.paramsChatbot,
-      name: nameRedux
-    })
-    const productWantToBuy = await this.createMessage.createAnswer(1)
+    await this.createMessage.createQuestion([
+      [
+        "text", `Hello, ${nameRedux}`
+      ],
+      [
+        "text", "What do you want to buy in my store?"
+      ],
+    ])
+    const productWantToBuy = await this.createMessage.createAnswer([
+      "multi-select",
+      [
+        {
+          value: 'woman_product_',
+          text: `Woman's outfit`,
+        },
+        {
+          value: 'man_product_',
+          text: `Man's outfit`,
+        },
+        {
+          value: 'bag_product_',
+          text: 'Bag',
+        },
+        {
+          value: 'shoes_product_',
+          text: 'Shoes',
+        },
+        {
+          value: 'watches_product_',
+          text: 'Watches',
+        },
+      ]
+    ])
 
     return productWantToBuy.options
   }
 
   async protocol2(productWantToBuyRedux) {
-    await this.createMessage.createQuestion(2, {
-      ...this.paramsChatbot,
-      productWantToBuy: productWantToBuyRedux
-    })
-    const confirm = await this.createMessage.createAnswer(2)
+    await this.createMessage.createQuestion([
+      [
+        "text", `OK, you want to buy ${productWantToBuyRedux.toString()}`
+      ],
+      [
+        "text", "Please confirm it!"
+      ]
+    ])
+    const confirm = await this.createMessage.createAnswer([
+      "select",
+      [
+        {
+          value: '-',
+          text: 'Yes'
+        },
+        {
+          value: '',
+          text: 'No'
+        },
+      ]
+    ])
 
     return !!confirm.option.value
   }
 
   async protocol3(productWantToBuyRedux) {
-    await this.createMessage.createQuestion(3, {
-      ...this.paramsChatbot,
-      productWantToBuy: productWantToBuyRedux
-    })
-    productWantToBuyRedux.forEach((msg) => {
-      window.open(link + msg.value + random(max));
-    })
+    await this.createMessage.createQuestion(createListMsgsChatbot(createLinkToProducts(productWantToBuyRedux.map(product => product.value), max), gotoLinkMsg, link)
+    )
+    // productWantToBuyRedux.forEach((msg) => {
+    //   window.open(link + msg.value + random(max));
+    // })
 
-    return await this.createMessage.createAnswer(3)
+    return await this.createMessage.createAnswer([
+      "multi-select",
+      [
+        {
+          value: 'buy_another',
+          text: `Buy another`,
+        }
+      ]
+    ])
   }
 }
